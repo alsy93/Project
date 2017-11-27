80d87790aac0c597c3fb948dbccdeb8e70f53a31
alsy93 <russo.aloisia@gmail.com>
2017-11-27 11:27:55 +0100

    modifiche di spostamento file e cancellazione astrocostant

---
+++ Script/Project.m
@@ -0,0 +1,12 @@
+%%SOYUZ DESCENT MODULE RE-ENTRY FIRST PHASE)
+%
+%
+%
+%
+
+addpath(genpath('..\Functions'))
+
+%ECI frame has the Gamma point direction and X-Y plane lies on the
+%equatorial plane, while the Z axis passes trought the North Pole.
+ECI = eye(3);
+
