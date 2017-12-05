ef0ac116637e3c76279175f3b0394e77f193ddd0
alsy93 <russo.aloisia@gmail.com>
2017-12-05 22:27:38 +0100

    cambio segni nelle equazioni

--- Script/Mechanicalsystm.m
+++ Script/Mechanicalsystm.m
@@ -7,9 +7,9 @@ lat = y(4);
 
 dydt = zeros(4,1);
 
-dydt(1,1) = -((par.rho_s*par.g)/(2*par.beta))*v^2 + par.g*sind(gamma);
-dydt(2,1) = -((par.rho_s*par.g)/(2*par.beta))*par.eff*v + (par.g*cosd(gamma)/v)  - v*cosd(gamma)/(par.Re+h);
-dydt(3,1) = -v*sind(gamma);
+dydt(1,1) = -((par.rho_s*par.g)/(2*par.beta))*v^2 - par.g*sind(gamma);
+dydt(2,1) = ((par.rho_s*par.g)/(2*par.beta))*par.eff*v - (par.g*cosd(gamma)/v)  - v*cosd(gamma)/(par.Re+h);
+dydt(3,1) = v*sind(gamma);
 dydt(4,1) = v*cosd(gamma)/(par.Re + h);
 
 end
