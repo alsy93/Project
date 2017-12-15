87647d13495455ec8aca31a786f5e8eedb1097f2
giuliabortolato <33417780+giuliabortolato@users.noreply.github.com>
2017-12-15 12:58:49 +0100

    Wall temperature

---
+++ Script/Functions/wall_temperature.m
@@ -0,0 +1,38 @@
+function [T_w, q_rad_TS] = wall_temperature(v, h, parT)
+
+% Function to calculate the wall temperature at the stagnation point.
+%
+% !!! NON ORTHODOX WAY
+% We correlate two equations, the analytic Stefan-Boltzman equation for
+% radiative heat flux and the statistical Sutton-Tauber one.
+%
+
+tic
+% Vectors initialization
+N = length(v);
+T_w = zeros(1,N);
+q_rad_TS = zeros(1,N);
+rho = varrho(h);
+
+%
+for i=1:N
+    q_rad_TS(i) = parT.C * Rn^parT.a * rho(i)^parT.b * v(i)^parT.d;
+    T_w(i) = (q_rad_TS(i) / (parT.sigma * parT.emiss))^(1/4);
+end
+toc=ram;
+
+% Find the corresponding density at each evaluation
+
+    function rho = varrho(h)
+        if h > 11 %after tropopause
+              h = h*1e3;
+              [rho,~, ~, ~, ~, ~] = atmos(h);   %UM: [kg/m^3]
+              rho = rho*1e9;                    %UM: [kg/km^3]
+        else 
+              h = h*1e3;
+              [rho,~, ~, ~,~]=tropos(h);        %UM: [kg/m^3]
+              rho = rho*1e9;                    %UM: [kg/km^3]
+        end
+    end
+
+end
